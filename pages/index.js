c7f1a8221cbaeddb631f644ac3323353cf85b556
Eduardo Borio <eduborio@gmail.com>
2023-08-13 16:55:40 +0000

    adicionando link de download da apresentação

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 export default function Home() {
-  return;
-  <>
-    <h1> Apresentação - Arthur Borio</h1>
-    <div>
+  return (
+    <>
+      <h1>Apresentação do Arthur</h1>
       <a href="/teste.pptx" download="arthur-borio">
         Download da Apresentação
       </a>
-    </div>
-  </>;
+    </>
+  );
 }
